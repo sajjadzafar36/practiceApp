76bdf7feecdb43f867b3eed1ce63638493b73d9e
Sajjad Zafar <sajjad@zafarsithub.com>
2020-06-19 15:06:43 +0200

    basic app commited

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res){
+    res.render('index');
+});
+
+app.get("/about", function(req, res){
+    res.render('about');
+});
+
+
+app.listen(3000, function(){
+    console.log("Server has started");
+});
